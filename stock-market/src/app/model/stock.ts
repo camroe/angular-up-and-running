391ecf7205c3afe958ede360b24b39f4891c1b83
Cam Roe <camroe@gmail.com>
2018-05-11 12:29:56 -0600

    Working - first part of chapter 3, non-array stock.

--- stock-market/src/app/model/stock.ts
+++ stock-market/src/app/model/stock.ts
@@ -1,6 +1,6 @@
 export class Stock {
   favorite: boolean = false;
-
+  classes: string;
 // Using TypeScript's syntactic suger to make public properties in the constructor 'properties'
   /* In short, we have created a class with five properties, four coming through the constructor and one auto-initialized.
    */
@@ -13,4 +13,27 @@ export class Stock {
   isPositiveChange(): boolean {
     return this.price > this.previousPrice;
   }
+
+  isSmallChange(): boolean {
+    return (Math.abs((this.price / this.previousPrice) - 1) <= 0.01);
+  }
+
+  isLargeChange(): boolean {
+    return (Math.abs((this.price / this.previousPrice) - 1) > 0.01);
+  }
+
+  getClasses(): string {
+    this.classes = " {" + "\"positive\":"
+      + this.isPositiveChange()
+      + ",\"negative\":" +
+      !this.isPositiveChange()
+      + ",\"large-change\":"
+      + this.isLargeChange()
+      + ",\"small-change\":"
+      + this.isSmallChange()
+      + "}";
+
+    return this.classes;
+  }
 }
+
--- stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -11,29 +11,33 @@ import {Stock} from '../../model/stock';
 export class StockItemComponent implements OnInit {
 
 public stocks: Array<Stock>;
-// public stockClasses; //What class is this then?
+public stock: Stock;
+
+public stockClasses; //What class is this then?
 public stockStyles;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.stocks = [
-      new Stock('Cam\'s Test Stock', "CTS", 150,200),
-      new Stock('Leslie Stock', "LTS", 20,184),
-      new Stock('Will Test Stock', "WTS", 15,184),
-      new Stock('Sam Test Stock', "STS", 200,100)
-      ];
+
+this.stock = new Stock('test Stock Company', 'TSC', 85,84);
+    // this.stocks = [
+    //   new Stock('Cam\'s Test Stock', "CTS", 150,200),
+    //   new Stock('Leslie Stock', "LTS", 20,184),
+    //   new Stock('Will Test Stock', "WTS", 15,184),
+    //   new Stock('Sam Test Stock', "STS", 200,100)
+    //   ];
 
     // let diff = (this.stock.price / this.stock.previousPrice) - 1;
     // let largeChange = Math.abs(diff) > 0.01; // 1% ?
 
-    // this.stockClasses = {
-    //   "positive":this.stock.isPositiveChange(),
-    //   "negative":!this.stock.isPositiveChange(),
-    //   "large-change":largeChange,
-    //   "small-change": !largeChange
-    // };
+    this.stockClasses = {
+      "positive":this.stock.isPositiveChange(),
+      "negative":!this.stock.isPositiveChange(),
+      "large-change":this.stock.isLargeChange(),
+      "small-change": this.stock.isSmallChange()
+    };
     // this.stockStyles = {
     //   "color": this.stock.isPositiveChange() ? "green" : "red",
     //   "font-size": largeChange ? "1.2em" : "0.8em"
@@ -44,6 +48,7 @@ public stockStyles;
     console.log('We are toggling the favorite state for this stock:',index,event,this.stocks[index].favorite);
     this.stocks[index].favorite = !this.stocks[index].favorite;
     console.log('After the toggle, the flag is ', this.stocks[index].favorite);
+    console.log (this.stocks[index].getClasses());
   }
 
   trackStockByCode(index,stock) {
